39c54b115000dd5acfc940b16aea7bdd2270bf3d
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2024-05-22 11:15:58 -0300

    Get periods from the API

--- main.pl
+++ main.pl
@@ -15,6 +15,7 @@ use POSIX "strftime";
 my $school_slug = "acadiau";
 my $location_name = "Wheelock Dining Hall";
 
+# User Agent to get data from the API
 my $ua = LWP::UserAgent->new;
 $ua->ssl_opts(verify_hostname => 0);
 
@@ -55,29 +56,59 @@ sub get_location_id {
   }
 }
 
-# Get the api json as a hash reference
+# Get the API JSON as a hash reference
 sub get_api {
-  my ($school_id, $location_id) = @_;
+  my ($school_id, $location_id, $period) = @_;
 
+  # Breakfast, lunch, or dinner. Default is ""
+  $period ||= "";
+  
+  # Load testing dataset, temporary
+  my $test_data = shift;
+  if (defined $test_data) {
+    open(my $fh, "<", "test-data.json") or die "$!";
+    my $text = join("", <$fh>);
+    return decode_json $text;
+  }
+  
   # Breakfast, lunch, dinner, first period is ""
-  my $period = "";
+  #my $period = "";
   
   my $date = strftime "%Y%m%d", localtime; # "yyyymmdd" format
   
   get_json "https://api.dineoncampus.ca/v1/location/$location_id/periods/$period?platform=0&date=$date";
 }
 
-# Get the categories of the menu from the api as a hash reference
+# Get all of the period names and IDs
+sub get_periods {
+  my $api = shift;
+  
+  my $api_periods = $api->{periods};
+
+  my @periods;
+
+  foreach (@$api_periods) {
+    my %period = (
+      name => $_->{name},
+      id => $_->{id}
+    );
+    
+    push(@periods, \%period);
+  }
+  @periods;
+}
+
+# Get the categories of the menu from the API as a hash reference
 sub get_menu {
   my $api = shift;
 
   my $readable_date = strftime "%d-%m-%Y", localtime;
-
+  
   # Check if it's closed
-  #if ($api->{closed} == 1) {
-  #  say "Wheelock hall is closed on $readable_date.";
-  #  exit 0;
-  #}
+  if ($api->{closed} == 1) {
+    say "Wheelock hall is closed on $readable_date.";
+    exit 0;
+  }
   
   # Check if a menu is avaliable
   die "No menu avaliable for $readable_date" unless defined $api->{menu};
@@ -86,37 +117,43 @@ sub get_menu {
   my $categories = $api->{menu}->{periods}->{categories};
 }
 
-sub print_category {
-  my $category = shift;
+sub print_menu {
+  my $categories = shift;
 
-  say $category->{name};
-
-  my $items = $category->{items};
+  my $readable_date = strftime "%d-%m-%Y", localtime;
   
-  foreach (@$items) {
-    say "- " . $_->{name};
-  }
-}; # <-- Strange semicolon to fix Emacs indenting
+  say "Menu for $readable_date:";
 
-# Load the test-data.json set
-sub TESTING_api {
-  open(my $fh, "<", "test-data.json") or die "$!";
-  my $text = join("", <$fh>);
-  decode_json $text;
+  foreach (@$categories) {
+    say "\n$_->{name}";
+    my $items = $_->{items};
+
+    foreach (@$items) {
+      say "- " . $_->{name};
+    }
+  }
 }
 
 sub main {
   my $school_id = get_school_id;
   my $location_id = get_location_id $school_id;
 
-  #my $api = get_api $school_id, $location_id;
-  my $api = TESTING_api;
+  # Get API first to determine the different periods and IDs
+  # TODO: Store periods client-side so we only need to do this once
+  my @periods = get_periods (get_api $school_id, $location_id, "TESTING");
+
+  foreach (@periods) {
+    say $_->{id} . ", " . $_->{name};
+  }
+  print "\n";
+
+  # Get API again with period id
+  # TODO: Let user use command line args to choose a period
+  my $api = get_api $school_id, $location_id, $periods[0], "TESTING";
   
   my $categories = get_menu $api;
 
-  foreach (@$categories) {
-    print_category $_;
-  }
+  print_menu $categories;
 }
 
 main;
